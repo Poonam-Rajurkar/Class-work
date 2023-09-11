0aa71698c31d9abb13ffe91de9d70fb0c988de3e
Poonam-Rajurkar <144650877+Poonam-Rajurkar@users.noreply.github.com>
2023-09-11 11:49:17 +0530

    Java Addition Program

---
+++ Add.java
@@ -0,0 +1,11 @@
+// Program for addition of two numbers.
+class Add
+{
+	public static void main(String args[])
+	{
+		int num1=25, num2=35, sum;    //variable declaration
+		sum = num1 + num2;
+		System.out.println("Addition = " + sum);
+		System.out.println("Addition of "+ num1 + " and " + num2 + " is " + sum);
+	}
+}
