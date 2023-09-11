9e192ad9829b7c221de4398abec7b86e9575b36b
Poonam-Rajurkar <144650877+Poonam-Rajurkar@users.noreply.github.com>
2023-09-11 11:48:15 +0530

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,15 @@
+// Program to print details about myself.
+class AboutMe
+{
+	public static void main(String args[])
+	{
+		String name = "Poonam Rajurkar", edu = "B.E. in Computer Science Engineering",
+		skills= "C, C++, Core Java, HTML, CSS, Angular";
+		int age = 21;
+		
+		System.out.println("Name : " + name);
+		System.out.println("Age : " + age);
+		System.out.println("Education : " + edu);
+		System.out.println("Skills : " + skills);
+	}
+}
